cf7f11b663e0e9a606b0bbf8baca708e60863b3c
S9ten <71446710+S9ten@users.noreply.github.com>
2022-12-28 16:52:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
+
 def load_image(name: str, colorkey=None) -> pygame.Surface:
     fullname = os.path.join('data', name)
     # если файл не существует, то выходим
@@ -39,7 +40,61 @@ def load_image(name: str, colorkey=None) -> pygame.Surface:
     else:
         image = image.convert_alpha()
     return image
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
 
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        if pygame.key.get_pressed()[pygame.K_1]:
+            self.cut_sheet(load_image("weapon_walk.png"), 2, 1)
+            if pygame.key.get_pressed()[pygame.K_DOWN] and self.rect.y < HEIGHT - 261:
+                self.cur_frame = (self.cur_frame + 2) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.rect.y += 10
+            else:
+                self.image = load_image("chel2.png", -1)
+            if pygame.key.get_pressed()[pygame.K_UP] and self.rect.y > 0:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.rect.y += -10
+            if pygame.key.get_pressed()[pygame.K_LEFT] and self.rect.x > 0:
+                self.cur_frame = (self.cur_frame + 2) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.rect.x += -10
+        else:
+            if pygame.key.get_pressed()[pygame.K_DOWN] and self.rect.y < HEIGHT - 261:
+                self.cur_frame = (self.cur_frame + 2) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.rect.y += 10
+            else:
+                self.image = load_image("chel2.png", -1)
+            if pygame.key.get_pressed()[pygame.K_UP] and self.rect.y > 0:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.rect.y += -10
+            if pygame.key.get_pressed()[pygame.K_LEFT] and self.rect.x > 0:
+                self.cur_frame = (self.cur_frame + 2) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.rect.x += -10
+
+        if pygame.key.get_pressed()[pygame.K_RIGHT] and self.rect.x < WIDTH - 105:
+            self.cur_frame = (self.cur_frame + 2) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
+            self.rect.x += 10
 
 class Camera:
     def __init__(self):
@@ -55,43 +110,6 @@ class Camera:
         self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    image = load_image("chel2.png")
-
-    def __init__(self, pos):
-        super().__init__(all_sprites)
-        self.image = AnimatedSprite.image
-        self.rect = self.image.get_rect()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
-        self.frame_count = 0
-
-    def update(self, *args):
-        self.frame_count += 1
-        if pygame.key.get_pressed()[pygame.K_DOWN] and self.rect.y < HEIGHT - 261:
-            self.rect.y += 5
-        if pygame.key.get_pressed()[pygame.K_UP] and self.rect.y > 0:
-            self.rect.y += -10
-        if pygame.key.get_pressed()[pygame.K_LEFT] and self.rect.x > 0:
-            self.rect.x += -10
-        if pygame.key.get_pressed()[pygame.K_RIGHT] and self.rect.x < WIDTH - 105:
-            self.rect.x += 5
-            print(self.frame_count)
-            if self.frame_count == 1:
-                self.image = load_image("chelhod1.png")
-
-            elif self.frame_count == 2:
-                self.image = load_image("chelhod3.png")
-            elif self.frame_count == 3:
-                self.image = load_image("chelhod4.png")
-            else:
-                self.image = load_image("chelhod2.png")
-                self.frame_count = 0
-        else:
-            self.image = load_image("chel2.png")
-            self.frame_count = 0
-
-
 class Border(pygame.sprite.Sprite):
     # строго вертикальный или строго горизонтальный отрезок
     def __init__(self, x1, y1, x2, y2):
@@ -105,6 +123,8 @@ class Border(pygame.sprite.Sprite):
             self.add(horizontal_borders)
             self.image = pygame.Surface([x2 - x1, 1])
             self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
 class Mountain(pygame.sprite.Sprite):
     image = load_image("img.png")
 
@@ -116,6 +136,7 @@ class Mountain(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         # располагаем горы внизу
         self.rect.bottom = HEIGHT
+
 
 if __name__ == '__main__':
 
@@ -131,7 +152,7 @@ if __name__ == '__main__':
     Border(WIDTH - 5, 5, WIDTH - 5, HEIGHT - 5)
     camera = Camera()
     mountain = Mountain()
-    player = AnimatedSprite((50, 50))
+    player = AnimatedSprite(load_image("walk2.png"), 3, 1, 50, 50)
     while running:
 
         for event in pygame.event.get():
@@ -140,7 +161,7 @@ if __name__ == '__main__':
         camera.update(player)
         for sprite in all_sprites:
             camera.apply(sprite)
-        tick = clock.tick(30)
+        tick = clock.tick(5)
         screen.fill(BLACK)
         all_sprites.draw(screen)
         all_sprites.update()
